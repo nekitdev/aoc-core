84087954c4e7c9782c1a63dc3bcda53fd12b92fc
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-04-06 23:23:49 +0300

    Prepare the next release.

--- aoc/names.py
+++ aoc/names.py
@@ -7,14 +7,21 @@ from aoc.primitives import Day, Key, Year
 __all__ = ("get_key_by_name", "get_name_by_key")
 
 YEAR: Literal["year"] = "year"
-DAY: Literal["day"] = "day"
+"""The `year` literal."""
 
-NAME_PATTERN = r"^Year(?P<year>[0-9]{4})Day(?P<day>[0-9]{2})$"
+DAY: Literal["day"] = "day"
+"""The `day` literal."""
+
+YEAR_TITLE = YEAR.title()
+"""The `Year` literal."""
+DAY_TITLE = DAY.title()
+"""The `Day` literal."""
+
+NAME_PATTERN = rf"^{YEAR_TITLE}(?P<{YEAR}>[0-9]{{4}}){DAY_TITLE}(?P<{DAY}>[0-9]{{2}})$"
 
 NAME = compile(NAME_PATTERN)
 
-NAME_EXAMPLE = "YearYYYYDayDD"
-"""The example of the name format."""
+NAME_EXAMPLE = f"{YEAR_TITLE}YYYY{DAY_TITLE}DD"
 
 INVALID_NAME = f"invalid name `{{}}`; expected `{NAME_EXAMPLE}` format"
 invalid_name = INVALID_NAME.format
@@ -22,15 +29,15 @@ invalid_name = INVALID_NAME.format
 INVALID_NAME_WITH_REASON = "invalid name `{}` ({})"
 invalid_name_with_reason = INVALID_NAME_WITH_REASON.format
 
-NAME_MATCHED_BUT_NO_YEAR = "name matched but the year group is not set"
-NAME_MATCHED_BUT_NO_DAY = "name matched but the day group is not set"
+NAME_MATCHED_BUT_NO_YEAR = "name matched but the `year` group was not found"
+NAME_MATCHED_BUT_NO_DAY = "name matched but the `day` group was not found"
 
 
 def get_key_by_name(name: str) -> Key:
     """Gets the key representing the problem by the name of the solution type.
 
     Note:
-        The `name` must match the format of [`NAME_EXAMPLE`][aoc.names.NAME_EXAMPLE].
+        The `name` must match the format of `YearYYYYDayDD`.
 
     Arguments:
         name: The name of the solution type.
@@ -51,14 +58,14 @@ def get_key_by_name(name: str) -> Key:
     year_option = match.group(YEAR)
 
     if year_option is None:
-        raise InternalError  # TODO: message?
+        raise InternalError(NAME_MATCHED_BUT_NO_YEAR)
 
     year_value = int(year_option)
 
     day_option = match.group(DAY)
 
     if day_option is None:
-        raise InternalError  # TODO: message?
+        raise InternalError(NAME_MATCHED_BUT_NO_DAY)
 
     day_value = int(day_option)
 
